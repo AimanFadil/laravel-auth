d38778b5d024920ca4b853879732b22fe182c27d
AimanFadil <aimanbello2001@gmail.com>
2024-02-21 18:08:03 +0100

    show element in index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,8 +3,37 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-12 text-center">
+                <h1>Projects</h1>
+            </div>
             <div class="col-12">
-                <h1>Ciao</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Programma</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Emulazione</th>
+                            <th scope="col">Descrizione</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <th>{{ $project->id }}</th>
+                                <td>{{ $project->nome }}</td>
+                                <td>{{ $project->slug }}</td>
+                                <td>{{ $project->programma }}</td>
+                                <td>{{ $project->data }}</td>
+                                <td>{{ $project->emulazione }}</td>
+                                <td>{{ $project->descrizione }}</td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
